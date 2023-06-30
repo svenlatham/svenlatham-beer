e2d1230927b9201f7df0b40f3ccd7100310bb934
Sven Latham <sven@noggin.bi>
2023-06-30 23:46:44 +0100

    run container in background so we can wget it

--- build.bash
+++ build.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 # We're going to use this to build a local output in _build/
-docker build -t svenlatham-beer . && docker run -p 8087:8087 svenlatham-beer
+docker build -t svenlatham-beer . && docker run -d -p 8087:8087 svenlatham-beer
 wget -r -nH -P "./_site/" "http://localhost:8087"
