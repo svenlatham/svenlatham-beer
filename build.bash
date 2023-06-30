a65c63acdee80c7272045c9f97f6f1b00521f143
Sven Latham <sven@noggin.bi>
2023-06-30 23:43:47 +0100

    Fixed incorrect tag name for docker

--- build.bash
+++ build.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 # We're going to use this to build a local output in _build/
-docker build -t svenlatham-beer . && docker run -p 8087:8087 -v .:/app/ localhost/svenlatham-beer
+docker build -t svenlatham-beer . && docker run -p 8087:8087 -v .:/app/ svenlatham-beer
 wget -r -nH -P "./_site/" "http://localhost:8087"
