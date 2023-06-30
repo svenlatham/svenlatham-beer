b3917f065202b828cb9340af3cf40d8d69927bab
Sven Latham <sven@noggin.bi>
2023-07-01 00:07:20 +0100

    Trying to get logs back from server

--- build.bash
+++ build.bash
@@ -1,4 +1,7 @@
 #!/bin/bash
 # We're going to use this to build a local output in _build/
-docker build -t svenlatham-beer . && docker run -d -p 8087:8087 svenlatham-beer
+docker build -t svenlatham-beer .
+docker run -d -p 8087:8087 svenlatham-beer
+sleep 5
+docker logs svenlatham-beer
 wget -r -nH -P "./_site/" "http://localhost:8087" || exit 0
