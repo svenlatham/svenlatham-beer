eba45cad7b876d6a202c820c39f77d6832a2fa05
Sven Latham <sven@noggin.bi>
2023-07-01 00:09:27 +0100

    Generation of thumbs should be a build task

--- build.bash
+++ build.bash
@@ -2,6 +2,4 @@
 # We're going to use this to build a local output in _build/
 docker build -t svenlatham-beer .
 docker run -d -p 8087:8087 svenlatham-beer
-sleep 5
-docker logs svenlatham-beer
 wget -r -nH -P "./_site/" "http://localhost:8087" || exit 0
--- run.bash
+++ run.bash
@@ -1,7 +1,4 @@
 #!/bin/bash
-echo "Generating thumbs"
-python generate-thumbs.py
 export FLASK_APP=www
 export FLASK_DEBUG=1
-echo "Running Flask app"
 flask run --host=0.0.0.0 --port=8087
