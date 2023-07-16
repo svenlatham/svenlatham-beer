3567822d4cd0fb576a695afa8785238527adc822
Sven Latham <sven@noggin.bi>
2023-07-16 21:20:25 +0100

    Enabled local test with podman or docker

--- test.bash
+++ test.bash
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-podman build -t svenlatham-beer . && podman run -it -p 8087:8087 -v .:/app/ localhost/svenlatham-beer
+# if podman is installed, use that, otherwise use docker
+if command -v podman &> /dev/null
+then
+    echo "Using podman"
+    # resisting aliases here in case we have variation in the config
+    podman build -t svenlatham-beer . && podman run -it -p 8087:8087 -v .:/app/ localhost/svenlatham-beer
+else
+    echo "Using docker"
+    docker build -t svenlatham-beer . && docker run -it -p 8087:8087 -v .:/app/ localhost/svenlatham-beer
+fi
