8831478b53990b1a213a8ff0575c8c4653b08b3e
Sven Latham <sven@svenlatham.com>
2023-08-26 21:17:55 +0100

    wget should now report content errors

--- build.bash
+++ build.bash
@@ -2,4 +2,4 @@
 # We're going to use this to build a local output in _build/
 docker build -t svenlatham-beer .
 docker run -d -p 8087:8087 svenlatham-beer
-wget -r -nH -P "./_site/" "http://localhost:8087" || exit 0
+wget -r --content-on-error -nH -P "./_site/" "http://localhost:8087" || exit 0
