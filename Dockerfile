ef92dcbb5d788a66886390fe4b2d4fea41f9de3f
Sven Latham <sven@noggin.bi>
2025-08-18 21:30:56 +0000

    Cleared up some old generation files

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM docker.io/library/python:3
+FROM docker.io/library/python:latest
 WORKDIR /app/
 COPY requirements.txt /app/
 RUN pip install --no-cache-dir -r requirements.txt
--- test.bash
+++ test.bash
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-# if podman is installed, use that, otherwise use docker
-if command -v podman &> /dev/null
-then
-    echo "Using podman"
-    # resisting aliases here in case we have variation in the config
-    podman build -t svenlatham-beer . && podman run -it -p 8087:8087 -v .:/app/ localhost/svenlatham-beer
-else
-    echo "Using docker"
-    docker build -t svenlatham/svenlatham-beer . && docker run -it -p 8087:8087 -v $(pwd):/app/ svenlatham/svenlatham-beer
-fi
+docker build -t svenlatham/svenlatham-beer . && docker run -it -p 8087:8087 -v $(pwd):/app/ svenlatham/svenlatham-beer
+
